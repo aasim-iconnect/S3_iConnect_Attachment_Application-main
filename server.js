89747aa8a5c54742a59c1da8405db3a4537e9e10
Aditya Tawade <adityatwd0@gmail.com>
2022-06-12 02:03:35 +0530

    Removed Short URl

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const AWS = require("aws-sdk");
-var shortUrl = require("node-url-shortener");
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -10,10 +9,6 @@ const fs = require("fs");
 
 const fileUrl =
   "http://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
-
-shortUrl.short(fileUrl, function (err, url) {
-  console.log(url);
-});
 
 const file = fs.createWriteStream("001.jpg");
 const request = http.get(fileUrl, function (response) {
