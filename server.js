c705eba88f459c516adc90012d06cc63404f581a
Aditya Tawade <adityatwd0@gmail.com>
2022-06-12 01:48:33 +0530

    remove gitignore files

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const AWS = require("aws-sdk");
+var shortUrl = require("node-url-shortener");
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
+const http = require("http"); // or 'https' for https:// URLs
+const fs = require("fs");
 
-export const generatePreSignedPutUrl = async (fileName, fileType) => {
-  const params = {
-    Bucket: bucketName,
-    Key: fileName,
-    Expires: 60,
-  };
+const fileUrl =
+  "http://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
 
-  let url;
-  try {
-    url = await s3.getSignedUrlPromise("putObject", params);
-  } catch (err) {
-    // do something with the error here
-    // and abort the operation.
-    return;
-  }
+shortUrl.short(fileUrl, function (err, url) {
+  console.log(url);
+});
 
-  return url;
-};
+const file = fs.createWriteStream("001.jpg");
+const request = http.get(fileUrl, function (response) {
+  response.pipe(file);
+
+  // after download completed close filestream
+  file.on("finish", () => {
+    file.close();
+    console.log("Download Completed");
+  });
+});
